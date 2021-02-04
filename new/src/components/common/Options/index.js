d40cb2e2cd5932977ab173ba4c595083b39877a3
stinkymonkeyph <monkeydev.team@gmail.com>
2021-02-05 00:03:06 +0800

    build dev and production scripts

---
+++ new/src/components/common/Options/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Options = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello World Options</h1>
+    </React.Fragment>
+  )
+}
+
+export default Options
---
+++ new/src/components/common/Popup/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello World Popup</h1>
+    </React.Fragment>
+  )
+}
+
+export default Popup
