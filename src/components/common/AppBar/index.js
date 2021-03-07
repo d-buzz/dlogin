d53a3cd7143417db19bc37825f3ba7dd4f2c7df1
stinkymonkeyph <monkeydev.team@gmail.com>
2021-03-08 00:09:51 +0800

    add api selection

--- src/components/common/AppBar/index.js
+++ src/components/common/AppBar/index.js
@@ -3,13 +3,23 @@ import MuiAppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar'
+import Chip from '@material-ui/core/Chip'
+
+import PublicIcon from '@material-ui/icons/Public'
 
 import {createUseStyles} from 'react-jss'
-import PropTypes from 'prop-types'
 
 const useStyles = createUseStyles({
   title: {
     flexGrow: 1,
+  },
+  input: {
+    width: 100,
+    paddingRight: 10,
+  },
+  inputRoot: {
+    border: '1px solid white !important', 
+    borderRadius: '25px !important',
   },
 })
 
@@ -25,6 +35,14 @@ const AppBar = () => {
           <Typography variant="subtitle2" className={classes.title}>
             <b>DLOGIN</b>
           </Typography>
+          <Chip
+            icon={<PublicIcon />}
+            label="https://api.deathwing..."
+            clickable
+            color="secondary"
+            variant="outlined"
+            style={{ marginRight: 10, color: 'white', borderColor: 'white' }}
+          />
           <Avatar alt="Coffeebender" src="https://images.hive.blog/u/coffeebender/avatar/small" />
         </Toolbar>
       </MuiAppBar>
